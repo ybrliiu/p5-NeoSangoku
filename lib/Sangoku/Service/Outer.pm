f5a8b6026dde450cfd436c4e31fcb622ac1a0b3e
liiu <yliiu_nan-na@docomo.ne.jp>
2016-10-01 20:18:55 +0900

    * ability sum value change when change value

--- lib/Sangoku/Service/Outer.pm
+++ lib/Sangoku/Service/Outer.pm
@@ -11,9 +11,10 @@ package Sangoku::Service::Outer {
     state $model = $class->model('IconList')->new();
 
     return {
-      icons_iter   => $model->get_iter($page),
-      max_page     => $model->max_page,
-      current_page => $page,
+      icons_iter     => $model->get_iter($page),
+      max_page       => $model->max_page,
+      current_page   => $page,
+      ICONS_DIR_PATH => $model->ICONS_DIR_PATH,
     };
   }
 
